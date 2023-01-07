dbfd0c4ada6697959e0007021858f4713f58f8c5
damax87 <110097091+damax87@users.noreply.github.com>
2023-01-07 14:13:47 +0000

    Deploying to gh-pages from @ damax87/goit-js-hw-09@12a56e399389f9e76341c980ab7bbe78416b79d8 🚀

---
+++ 01-color-switcher.2a21a651.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;var d=void 0;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.2a21a651.js.map
